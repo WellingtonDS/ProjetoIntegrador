e0f04222b1b3be4219c72d1c6d6cd244a2441b7d
DinobergueViana <dinobergueviana@gmail.com>
2020-11-05 11:13:48 -0300

    definindo a rota do tipo post da pagina de login

--- routes/LoginRouter.js
+++ routes/LoginRouter.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const LoginController = require('../controllers/LoginController');
 
 router.get('/', LoginController.showLogin);
+router.post('/', LoginController.showLogin);
 
 module.exports = router;
