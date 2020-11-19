c6b5072f3fc67d5027e4754dea94250eed5ed44e
DinobergueViana <dinobergueviana@gmail.com>
2020-11-19 14:36:54 -0300

    validando os dados inseridos no formulario de login

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -1,4 +1,5 @@
-const {sequelize, Usuario, Professor, Aluno} = require('../models')
+const {sequelize, Usuario, Professor, Aluno} = require('../models');
+const {check, validationResult, body} = require('express-validator');
 
 const LoginController = {
     showLoginProfessor: (req, res) => {
@@ -8,20 +9,29 @@ const LoginController = {
         res.render('loginAluno');
     },
     logar: async (req, res) => {
+        let usuario = null;
+        let professor = null;
         let {email, senha} = req.body;
-        let usuario = await Usuario.findOne({
-            where:{
-                email
-            }
-        })
+        let listaDeErros = validationResult(req);
 
-        let professor = await Professor.findOne({
-            where:{
-                usuario_id: usuario.id 
-            },
-            include: 'usuario'
-        })
-        req.session.usuario = professor;
+        if(listaDeErros.isEmpty()){
+            usuario = await Usuario.findOne({
+                where:{
+                    email
+                }
+            });
+
+            professor = await Professor.findOne({
+                where:{
+                    usuario_id: usuario.id 
+                },
+                include: 'usuario'
+            })
+            req.session.usuario = professor;
+        }else{
+            res.render('loginProfessor', {errors: listaDeErros.errors})
+        }
+
         res.redirect('/professor');
         
         
--- routes/LoginRouter.js
+++ routes/LoginRouter.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 const LoginController = require('../controllers/LoginController');
 const validarLogin = require('../middleware/auth');
+const {check, validationResult, body} = require('express-validator');
 
 router.get('/professor', LoginController.showLoginProfessor);
-router.post('/professor', LoginController.logar);
+router.post('/professor', [
+    check('email').isEmail().withMessage('Digite um email válido.'), 
+    check('senha').isLength({min:6}).withMessage('O tamanho da senha deve conter no mínimo 6 caracteres.')], 
+    LoginController.logar);
 router.get('/aluno', validarLogin, LoginController.showLoginAluno);
 router.post('/aluno', validarLogin, LoginController.logar);
-
+ 
 module.exports = router;
