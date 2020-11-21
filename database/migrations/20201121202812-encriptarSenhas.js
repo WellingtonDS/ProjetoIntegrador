0fc4fa98ad39201e8af93700ad5c2bb21df1be58
DinobergueViana <dinobergueviana@gmail.com>
2020-11-21 17:32:10 -0300

    cria migration para encriptar senhas

---
+++ database/migrations/20201121202812-encriptarSenhas.js
@@ -0,0 +1,24 @@
+'use strict';
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return await queryInterface.bulkUpdate('usuarios', {senha:bcrypt.hashSync('123456', 10)})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return await queryInterface.bulkUpdate('usuarios', {senha: '123456'});
+  }
+};
