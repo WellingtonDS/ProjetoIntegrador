65fa727124bdf7b9c38a8b7ec5d2ae8b2016173e
DinobergueViana <dinobergueviana@gmail.com>
2020-11-23 16:25:42 -0300

    altera ProfessorRouter e professorController

--- controllers/ProfessorController.js
+++ controllers/ProfessorController.js
@@ -1,5 +1,4 @@
 const session = require("express-session");
-const {sequelize, Turma, ProfessorDisciplina, Disciplina} = require('../models');
 
 const ProfessorController = {   
     index: (req, res) => {
@@ -15,27 +14,7 @@ const ProfessorController = {
     },
     disciplinas: (req, res) => {
         res.render('./professor/disciplinas', {professor: req.session.usuario});
-    },
-    turmas: async (req, res) => {
-        
-        let professor = req.session.usuario;
-
-        // pesquisa turmas onde um professor leciona
-        let turmas = await Turma.findAll({
-            include: [{
-                model: ProfessorDisciplina, 
-                as: 'professores_disciplinas', 
-                where:{professor_id: professor.id},
-                include: [{
-                    model: Disciplina,
-                    as: 'disciplina'
-                }]
-            }]
-        });
-
-        console.log(turmas)
-        res.render('./professor/turmas', {professor, turmas});
-    }
+    }    
 }
 
 module.exports = ProfessorController;
--- routes/ProfessorRouter.js
+++ routes/ProfessorRouter.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const ProfessorController = require('../controllers/ProfessorController');
+const TurmaController = require('../controllers/TurmaController');
 const validarSession = require('../middlewares/auth')
 
 router.get('/', validarSession, ProfessorController.index);
 router.get('/perfil', validarSession, ProfessorController.perfil);
 router.get('/horarios', validarSession, ProfessorController.horarios);
-router.get('/turmas', validarSession, ProfessorController.turmas);
+router.get('/turmas', validarSession, TurmaController.listar);
+router.get('/turmas/:turma_id', validarSession, TurmaController.detalhes);
 router.get('/disciplinas', validarSession, ProfessorController.disciplinas);
 
 
