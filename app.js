c24b3c2a580a8adb0252d61c8e371dc918af70d2
DinobergueViana <dinobergueviana@gmail.com>
2020-11-24 01:07:11 -0300

    cria registro de aula

--- app.js
+++ app.js
@@ -20,9 +20,10 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.bodyParser());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(session({secret: 'Roceket School' }));
 app.use(methodOverride('_method'));
 
--- routes/ProfessorRouter.js
+++ routes/ProfessorRouter.js
@@ -2,14 +2,18 @@ const express = require('express');
 const router = express.Router();
 const ProfessorController = require('../controllers/ProfessorController');
 const TurmaController = require('../controllers/TurmaController');
-const validarSession = require('../middlewares/auth')
+const AulaController = require('../controllers/AulaController');
+const validarSession = require('../middlewares/auth');
 
 router.get('/', validarSession, ProfessorController.index);
 router.get('/perfil', validarSession, ProfessorController.perfil);
 router.get('/horarios', validarSession, ProfessorController.horarios);
 router.get('/turmas', validarSession, TurmaController.listar);
 router.get('/turmas/:turma_id', validarSession, TurmaController.detalhes);
+router.get('/turmas/:turma_id/:professor_disciplina_id/aulas/registrar', validarSession, AulaController.registro);
+router.post('/turmas/:turma_id/:professor_disciplina_id/aulas/registrar', validarSession, AulaController.registrar);
 router.get('/disciplinas', validarSession, ProfessorController.disciplinas);
+
 
 
 
