48c3854de0249ebb4200d4f08aab3568a6050a36
DinobergueViana <dinobergueviana@gmail.com>
2020-11-26 10:16:45 -0300

    corrige problema de carregamento do css

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 // app.use(express.bodyParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({secret: 'Roceket School' }));
 app.use(methodOverride('_method'));
 
