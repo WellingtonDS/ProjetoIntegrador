5d6a91321e9444e1854ab15cf57924861b3261a0
Wellington <w_santos1993@outlook.com>
2020-12-07 15:39:20 -0300

    criando seeder: disciplinas

---
+++ database/seeders/20201207183330-disciplinas.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+
+    await queryInterface.bulkInsert('disciplinas',
+      [
+        { nome: 'Arte ', descricao: null },
+        { nome: 'Biologia', descricao: null },
+        { nome: 'Ciências', descricao: null },
+        { nome: 'Educação Física', descricao: null },
+        { nome: 'Filosofia', descricao: null },
+        { nome: 'Física', descricao: null },
+        { nome: 'Geografia', descricao: null },
+        { nome: 'istória', descricao: null },
+        { nome: 'Língua Inglesa', descricao: null },
+        { nome: 'Língua Portuguesa', descricao: null },
+        { nome: 'Matemática', descricao: null },
+        { nome: 'Química', descricao: null },
+        { nome: 'Sociologia', descricao: null }
+      ]
+      , {});
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+
+    await queryInterface.bulkDelete('disciplinas', null, {});
+
+  }
+};
