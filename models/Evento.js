dc9696b707b4c0668f54bb6ee97685645e6dbf43
DinobergueViana <dinobergueviana@gmail.com>
2020-12-29 10:13:10 -0300

    adicionando model Evento

---
+++ models/Evento.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+    const evento = sequelize.define(
+        "Evento",
+        {
+            data: {
+                type: DataTypes.DATE,
+                allowNull: false
+            },
+            descricao: {
+                type: DataTypes.TEXT,
+                allowNull: false
+            }
+        },
+        {
+            tableName: "eventos",
+            timestamps: false
+        }
+        
+    );
+}
