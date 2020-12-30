6ca730ef0498b9a75a3361ae0abbc2f1448c2e0f
DinobergueViana <dinobergueviana@gmail.com>
2020-12-30 16:41:45 -0300

    cria metodo buscar no controller CalendarioEscolar

--- controllers/CalendarioEscolarController.js
+++ controllers/CalendarioEscolarController.js
@@ -1,6 +1,14 @@
+const { sequelize, Evento } = require('../models');
+
 const CalendarioEscolarController = {
     show: (req, res) => {
         res.render('calendarioEscolar')
+    },
+    buscar: async (req, res) => {
+        let eventos = [];
+
+        eventos = await Evento.findAll()
+        res.json(eventos);
     }
 }
 
