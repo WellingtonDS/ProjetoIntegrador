b5a35010d9433378bfc7b07796b75194022b3506
DinobergueViana <dinobergueviana@gmail.com>
2020-12-30 16:40:32 -0300

    cria rota calendarioEscolar/eventos

--- routes/CalendarioEscolarRouter.js
+++ routes/CalendarioEscolarRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const CalendarioEscolarController = require('../controllers/CalendarioEscolarController');
 
-router.get('/', CalendarioEscolarController.show)
+router.get('/', CalendarioEscolarController.show);
+router.get("/eventos", CalendarioEscolarController.buscar)
 
 module.exports = router;
