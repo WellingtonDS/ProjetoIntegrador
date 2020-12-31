5f9b286134ec704d5bdc19061f926fff975ddbfd
DinobergueViana <dinobergueviana@gmail.com>
2020-12-31 10:39:29 -0300

    modifica o metodo de busca no arquivo testeModeleventos.js

--- testes/testeModelEvento.js
+++ testes/testeModelEvento.js
@@ -1,14 +1,22 @@
 const {sequelize, Evento} = require('../models');
-var eventos = [];
-var eventosExtraidos = [];
+// var eventos = [];
+// var eventosExtraidos = [];
 
+// const buscarEventos = async () => {
+//     eventos = await Evento.findAll();
+//     for(let i=0; i < eventos.length; i++){
+//         eventosExtraidos.push(eventos[i].dataValues)
+//     }
+//     console.log(eventosExtraidos)
+//     sequelize.close()
+// }
+
+// buscarEventos();
+
+const { QueryTypes } = require('sequelize');
 const buscarEventos = async () => {
-    eventos = await Evento.findAll();
-    for(let i=0; i < eventos.length; i++){
-        eventosExtraidos.push(eventos[i].dataValues)
-    }
-    console.log(eventosExtraidos)
-    sequelize.close()
+    let eventos = await sequelize.query(`SELECT * FROM eventos WHERE month(data) = ${3}`, { type: QueryTypes.SELECT});
+    console.log(eventos)
 }
 
 buscarEventos();
