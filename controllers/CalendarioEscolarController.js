a75d446a0230ef7e7231d21680b7a9935628d59f
DinobergueViana <dinobergueviana@gmail.com>
2020-12-31 14:43:05 -0300

    definindo termos para busca de eventos no banco de dados

--- controllers/CalendarioEscolarController.js
+++ controllers/CalendarioEscolarController.js
@@ -1,15 +1,24 @@
 const { sequelize, Evento } = require('../models');
 
 const CalendarioEscolarController = {
-    show: (req, res) => {
-        res.render('calendarioEscolar')
-    },
-    buscar: async (req, res) => {
-        let eventos = [];
+    index: (req, res) => {
+        // recuperar o numero referente ao mes 
+        let mesAtual = req.params.mes;
 
-        eventos = await Evento.findAll()
-        res.json(eventos);
+        // buscar os eventos no banco de dados
+
+        // fazer tratamento de possiveis erros
+
+        // enviar os dados para a view
+
+        res.render('calendarioEscolar');
     }
+    // buscar: async (req, res) => {
+    //     let eventos = [];
+
+    //     eventos = await Evento.findAll()
+    //     res.json(eventos);
+    // }
 }
 
 module.exports = CalendarioEscolarController;
--- routes/CalendarioEscolarRouter.js
+++ routes/CalendarioEscolarRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const CalendarioEscolarController = require('../controllers/CalendarioEscolarController');
 
-router.get('/', CalendarioEscolarController.show);
-router.get("/eventos", CalendarioEscolarController.buscar)
+router.get('/:mes?', CalendarioEscolarController.index);
+// router.get("/eventos", CalendarioEscolarController.buscar)
 
 module.exports = router;
