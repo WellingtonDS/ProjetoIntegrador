48008e9b24b27acc4778c49aa8932726f02d9f0e
DinobergueViana <dinobergueviana@gmail.com>
2021-01-04 18:08:11 -0300

    corrige bug nos botoes next e previous

--- public/javascripts/eventos.js
+++ public/javascripts/eventos.js
@@ -4,32 +4,36 @@ const mesAnterior = document.getElementById('mesAnterior');
 const mesAtivo = document.getElementById('mesAtivo');
 const mesSeguinte = document.getElementById('mesSeguinte');
 const mes = document.getElementById('mes');
+const botaoLeft = document.getElementById("botaoLeft");
+const botaoRight = document.getElementById("botaoRight");
 
 // definindo o mes padrão que irá aparecer no primeiro carregamento
-const date = new Date
+const date = new Date;
+var mesAtual = date.getMonth();
 mesAtivo.innerText = meses[date.getMonth()];
 // console.log(`Mês atual: ${meses.indexOf(mesAtual.innerText)+1}`)
-buscarEventos(meses.indexOf(mesAtivo.innerText) + 1);
+buscarEventos(meses.indexOf(mesAtivo.innerText));
 
-var indice = 11;
+var indice = 0;
+var eventosDoMes = {};
+
 
 // comentar os resto do código
 function alterarMes(event) {
     // recupera apenas o botao que foi clicado
     let botao = event.target.className.baseVal.split("-")[2];
+    // let botao = event.target;
 
-    console.log(botao);
-
-    if(botao === "right"){
-        if(mesAtivo.innerText === "Dezembro"){
+    if(botao == "right"){
+        if(mesAtivo.innerText == "Dezembro"){
             indice = 0;
         } else {
             indice += 1;
         }
     }
 
-    if(botao === "left"){
-        if(mesAtivo.innerText === "Janeiro"){
+    if(botao == "left"){
+        if(mesAtivo.innerText == "Janeiro"){
             indice = 11;
         } else {
             indice -= 1;
@@ -37,18 +41,22 @@ function alterarMes(event) {
     }
 
     mesAtivo.innerText = meses[indice];
-    
+    exibirEventos(eventosDoMes)
 }
 
-mes.addEventListener('click', (event) => {
+botaoLeft.addEventListener('click', (event) => {
     alterarMes(event);
-    buscarEventos(meses.indexOf(mesAtivo.innerText) + 1);
+    buscarEventos(meses.indexOf(mesAtivo.innerText));
 });
 
+botaoRight.addEventListener('click', (event) => {
+    alterarMes(event);
+    buscarEventos(meses.indexOf(mesAtivo.innerText));
+});
 
-
+// busca os dados no back-end
 function buscarEventos(mes) {
-    fetch(`/calendarioEscolar/eventos`)
+    fetch(`/calendarioEscolar/${mes}`)
     .then((resposta) => {
         return resposta.json()
     })
@@ -57,10 +65,13 @@ function buscarEventos(mes) {
     })
 }
 
-
+// imprime os dados encontrados na view calendarioEscolar
+function exibirEventos(eventos){
+    console.log(eventos)  
+}
 
 /* 
-    O que preciso fazer nesse arquivo e no back-end:
+    O que precisa ser feito nesse arquivo e no back-end:
 
     1. criar rotas para acesso à tabela eventos;
     2. criar uma função para acessar a tabela eventos via fetch;
