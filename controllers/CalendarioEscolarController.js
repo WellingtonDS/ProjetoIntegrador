b73a54067e9df444492bfd0e6160730c8bb6392a
DinobergueViana <dinobergueviana@gmail.com>
2021-01-08 17:43:38 -0300

    envia o dia do mes para view calendarioEscolar

--- controllers/CalendarioEscolarController.js
+++ controllers/CalendarioEscolarController.js
@@ -3,12 +3,13 @@ const { QueryTypes } = require('sequelize');
 
 const CalendarioEscolarController = {
     index: async (req, res) => {
-        // recuperar o numero referente ao mes vindo da requisição
+        // recupera o numero referente ao mes vindo da requisição
         let diaMes = Number(req.params.mes);
         let mes = null;
         let eventosDoMes = null;
         let meses = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"];
         
+        // verica se diaMes é undefined
         if(!diaMes){
             diaMes = 0;
             mes = meses[diaMes];
@@ -20,7 +21,7 @@ const CalendarioEscolarController = {
         eventosDoMes = await sequelize.query(`SELECT * FROM eventos WHERE month(data) = ${diaMes+1}`, { type: QueryTypes.SELECT});
 
         // envia os dados para a view
-        res.render("calendarioEscolar",{eventosDoMes, mes})
+        res.render("calendarioEscolar",{eventosDoMes, diaMes, mes})
     }
 }
 
