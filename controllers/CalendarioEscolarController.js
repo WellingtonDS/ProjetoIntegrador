817623ae98af43e5cb73bbc5ef6f864ca522b8a7
DinobergueViana <dinobergueviana@gmail.com>
2021-01-08 15:00:53 -0300

    definindo o mes que sera exibido na view calendarioEscolar

--- controllers/CalendarioEscolarController.js
+++ controllers/CalendarioEscolarController.js
@@ -4,18 +4,23 @@ const { QueryTypes } = require('sequelize');
 const CalendarioEscolarController = {
     index: async (req, res) => {
         // recuperar o numero referente ao mes vindo da requisição
-        let mes = Number(req.params.mes);
+        let diaMes = Number(req.params.mes);
+        let mes = null;
         let eventosDoMes = null;
+        let meses = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"];
         
-        if(!mes){
-            mes = 0;
+        if(!diaMes){
+            diaMes = 0;
+            mes = meses[diaMes];
         }
+
+        mes = meses[diaMes]
         
         // busca os eventos no banco de dados com base no parametro disponivel na requisição
-        eventosDoMes = await sequelize.query(`SELECT * FROM eventos WHERE month(data) = ${mes+1}`, { type: QueryTypes.SELECT});
+        eventosDoMes = await sequelize.query(`SELECT * FROM eventos WHERE month(data) = ${diaMes+1}`, { type: QueryTypes.SELECT});
 
         // envia os dados para a view
-        res.render("calendarioEscolar",{eventosDoMes})
+        res.render("calendarioEscolar",{eventosDoMes, mes})
     }
 }
 
