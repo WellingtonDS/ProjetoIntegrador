2a8c83e9304e903b882d59a015125fb0b2f48595
DinobergueViana <dinobergueviana@gmail.com>
2021-01-11 17:41:08 -0300

    desabitando os botoes next e previous

--- public/javascripts/calendarioEscolar.js
+++ public/javascripts/calendarioEscolar.js
@@ -1,10 +1,27 @@
 // definindo variaveis que serão manipuladas
 let meses = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"];
 
+const mesAtivo = document.getElementById('mesAtivo');
+const mesAnterior = document.getElementById('mesAnterior');
+const mesSeguinte = document.getElementById('mesSeguinte');
+
 const botaoLeft = document.getElementById("botaoLeft");
 const botaoRight = document.getElementById("botaoRight");
 
 var diaMes = 0;
+
+window.onload = () => {
+    // remove o atributo href da tag a (botao esquerdo)
+    if(mesAtivo.innerText === "Janeiro"){
+        mesAnterior.removeAttribute('href');
+    }
+
+    // remove o atributo href da tag a (botao direito)
+    if(mesAtivo.innerText === "Dezembro"){
+        mesSeguinte.removeAttribute('href');
+    }
+
+}
 
 // comentar os resto do código
 function alterarMes(event) {
