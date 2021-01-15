a28057f8d9300b2654e8265c554944ea78294c0c
DinobergueViana <dinobergueviana@gmail.com>
2021-01-15 16:53:16 -0300

    cria model Admin

---
+++ models/Admin.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+  const admin = sequelize.define(
+    'Admin',
+    {
+      nome: DataTypes.STRING(100),
+      sobrenome: DataTypes.STRING(100),
+      endereco: DataTypes.STRING(100),
+      telefone: DataTypes.STRING(15),
+      usuario_id: DataTypes.INTEGER
+    },
+    {
+      tableName: "admin",
+      timestamps: false
+    }
+  );
+
+  admin.associate = (models) => {
+    admin.belongsTo(models.Usuario, {foreignKey: 'usuario_id', as:'usuario'})
+  }
+
+  return admin;
+}
