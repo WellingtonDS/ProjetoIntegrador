0573c37ccce8e5278adeacd1db593343f3783156
DinobergueViana <dinobergueviana@gmail.com>
2021-01-15 16:52:41 -0300

    cria migration: tabela admin

---
+++ database/migrations/20210115182841-create-admin.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+    await queryInterface.createTable('admin', 
+    { 
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      nome: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      sobrenome: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      endereco: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      telefone: {
+        type: Sequelize.STRING(15),
+        allowNull: false
+      },
+      usuario_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        foreignKey: true,
+        references: {
+          model: "usuarios",
+          key: "id"
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE"
+      }
+       
+    });
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+
+    await queryInterface.dropTable('admin');
+
+  }
+};
