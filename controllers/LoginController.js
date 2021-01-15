1d54608e5cc3674c756733575110afe7f97f52da
DinobergueViana <dinobergueviana@gmail.com>
2021-01-15 16:58:40 -0300

    cria metodos: showLoginAdmin e logarAdmin

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -1,13 +1,50 @@
-const {sequelize, Usuario, Professor, Aluno} = require('../models');
+const {sequelize, Usuario, Admin, Professor, Aluno} = require('../models');
 const {check, validationResult, body} = require('express-validator');
 const bcrypt = require('bcrypt');
 
 const LoginController = {
+    showLoginAdmin: (req, res) => {
+        res.render('loginAdmin');
+    },
     showLoginProfessor: (req, res) => {
         res.render('loginProfessor');
     },
     showLoginAluno: (req, res) => {
         res.render('loginAluno');
+    },
+    logarAdmin: async (req, res) => {
+        let usuario = null;
+        let admin = null;
+        let {email, senha} = req.body;
+        let listaDeErros = validationResult(req);
+
+        if(!listaDeErros.isEmpty()){
+            return res.render('loginAdmin', {errors: listaDeErros.errors});
+        }
+        
+        usuario = await Usuario.findOne({
+            where:{
+                email
+            }
+        });
+
+        if(!usuario || !bcrypt.compareSync(senha, usuario.senha)){
+            listaDeErros.errors.push({msg: 'Usuário ou senha inválido.'});
+        }
+
+        if(listaDeErros.isEmpty()){
+            admin = await Admin.findOne({
+                where:{
+                    usuario_id: usuario.id
+                },
+                include: 'usuario'
+            });
+
+            req.session.usuario = admin;
+            res.redirect('/admin');
+        }
+
+        res.render('loginAdmin', {errors: listaDeErros.errors})
     },
     logarProfessor: async (req, res) => {
         let usuario = null;
