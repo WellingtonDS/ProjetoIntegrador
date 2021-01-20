030b8630047ddf411c7072ecf4480b32cd5ae7c3
DinobergueViana <dinobergueviana@gmail.com>
2021-01-19 23:04:14 -0300

    altera seed tarefas

--- database/seeders/20210119201234-tarefas.js
+++ database/seeders/20210119201234-tarefas.js
@@ -4,7 +4,11 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('tarefas', [
       {data: "2021-01-15", descricao: "Cadastrar novas turmas", situacao: 0, admin_id: 1},
-      {data: "2021-01-15", descricao: "Cadastrar disciplinas", situacao: 0, admin_id: 1}
+      {data: "2021-01-15", descricao: "Cadastrar disciplinas", situacao: 0, admin_id: 1},
+      {data: "2021-01-15", descricao: "Ligar para pais de de alunos", situacao: 0, admin_id: 1},
+      {data: "2021-01-15", descricao: "Revisar endereços", situacao: 0, admin_id: 1},
+      {data: "2021-01-15", descricao: "Fazer relatório de turmas", situacao: 0, admin_id: 1},
+      {data: "2021-01-15", descricao: "Matricular alunos", situacao: 0, admin_id: 1},
     ], {});
   },
 
