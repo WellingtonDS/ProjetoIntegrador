c6a892ff7f6720efba9066b6aaaac638db3638b8
DinobergueViana <dinobergueviana@gmail.com>
2021-01-20 15:29:37 -0300

    remove relacionamento no model Admin

--- models/Admin.js
+++ models/Admin.js
@@ -16,7 +16,6 @@ module.exports = (sequelize, DataTypes) => {
 
   admin.associate = (models) => {
     admin.belongsTo(models.Usuario, {foreignKey: 'usuario_id', as:'usuario'});
-    admin.hasMany(models.Tarefa, {as: 'tarefas'});
   }
 
   return admin;
