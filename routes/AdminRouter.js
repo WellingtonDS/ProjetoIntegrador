ef2a809183774aac78d96dc5ad645f9e906c887d
DinobergueViana <dinobergueviana@gmail.com>
2021-01-21 15:11:09 -0300

    cria rotas para gerenciamento de turmas

--- routes/AdminRouter.js
+++ routes/AdminRouter.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const TarefaController = require('../controllers/AdminController/TarefaController');
+const TurmaController = require('../controllers/AdminController/TurmaController');
 const {check, validationResult, body} = require('express-validator');
 const validarSession = require('../middlewares/auth');
 
 router.get('/', validarSession, TarefaController.index);
 router.post('/tarefas/criar', validarSession, TarefaController.criar);
 router.put('/tarefas/editar', validarSession, TarefaController.editar);
+
+router.get('/turmas/criar', validarSession, TurmaController.show);
+router.post('/turmas/criar', validarSession, TurmaController.criar);
 
 module.exports = router;
