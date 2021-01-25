fa347f8fa9dafbe06124e12336b6fc326d69f5a6
WellingtonDS <w_santos1993@outlook.com>
2021-01-25 16:51:09 -0300

    permite salvar alteracões na sessão admin

--- controllers/AdminController/TarefaController.js
+++ controllers/AdminController/TarefaController.js
@@ -4,12 +4,12 @@ const { sequelize, Tarefa } = require('../../models')
 const TarefaController = {
   index: async (req, res) => {
     let tarefas = await Tarefa.findAll(
-      {where:{situacao: false}}
+      { where: { situacao: false } }
     );
-    res.render('./admin/index', {admin: req.session.usuario, tarefas})
+    res.render('./admin/index', { admin: req.session.usuario, tarefas })
   },
   criar: async (req, res) => {
-    let {data, descricao} = req.body;
+    let { data, descricao } = req.body;
     let situacao = false;
     let admin_id = 1;
 
@@ -20,27 +20,30 @@ const TarefaController = {
       admin_id
     }
 
-    if(!data || !descricao){
-      res.status(401).json({erro: 'Não foi possível criar uma nova tarefa.'})
+    if (!data || !descricao) {
+      res.status(401).json({ erro: 'Não foi possível criar uma nova tarefa.' })
     }
-    
+
     res.status(200).json(tarefa)
   },
   alterar: async (req, res) => {
     let tarefas = req.body;
     let tarefasKeys = Object.keys(tarefas).length;
-    // for(var id in tarefas){
-    //   Tarefa.update(
-    //     {situacao: 1},{where: {id}}
-    //   )
-    // }
-    
-    if(!tarefasKeys){
-      res.status(401).json({erro: 'Não foi possível fazer alterações.'})
+
+    console.log(tarefas)
+    if (!tarefasKeys) {
+      res.status(401).json({ erro: 'Não foi possível fazer alterações.' })
+    }
+
+    for (var id in tarefas) {
+      Tarefa.update(
+        { situacao: "true" }, { where: { id:id } }
+      )
     }
     
+    res.redirect("/admin");
 
-    res.status(200).json(tarefas)
+    // res.status(200).json(tarefas);
   }
 }
 
