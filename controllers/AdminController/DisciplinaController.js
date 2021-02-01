8c6c7d2d80d3b1ea6645214681e9de7aabdb1728
DinobergueViana <dinobergueviana@gmail.com>
2021-02-01 17:41:20 -0300

    cria metodos CRUD: disciplinas

---
+++ controllers/AdminController/DisciplinaController.js
@@ -0,0 +1,40 @@
+const session = require('express-session');
+const { sequelize, Disciplina } = require('../../models');
+
+const DisciplinaController = {
+  index: async (req, res) => {
+    let disciplinas = await Disciplina.findAll();
+
+    res.status(200).json(disciplinas);
+  },
+  detalhes: async (req, res) => {
+    let {id} = req.params;
+    let disciplina = await Disciplina.findByPk(id)
+    res.status(200).json(disciplina)
+  },
+  editar: async (req, res) => {
+    let {id} = req.params;
+    let {nome, descricao, professor} = req.body;
+    
+    let disciplina = await Disciplina.findByPk(id);
+    if(!disciplina){
+      return res.status(401).json({msg: "Erro ao tentar atualizar Disciplina"})
+    }
+
+    Disciplina.update({nome:nome, descricao: descricao},{where: {id: id}})
+    res.redirect('/admin')
+  },
+  deletar: async (req, res) => {
+    let {id} = req.params;
+    let disciplina = await Disciplina.findByPk(id);
+
+    if(!disciplina){
+      res.status(401).json({msg:"Não foi possível concluir esta ação"})
+    }
+
+    await Disciplina.destroy({where: {id: id}})
+    res.redirect('/admin')
+  }
+}
+
+module.exports = DisciplinaController;
