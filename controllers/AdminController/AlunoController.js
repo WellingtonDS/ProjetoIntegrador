46b5edf27992e9dc6929dd9cffe8bf89a0637664
DinobergueViana <dinobergueviana@gmail.com>
2021-02-06 12:25:10 -0300

    cria metodo que permite buscar por um unico aluno

--- controllers/AdminController/AlunoController.js
+++ controllers/AdminController/AlunoController.js
@@ -1,16 +1,35 @@
 const session = require('express-session');
 const { sequelize, Aluno, Usuario } = require('../../models');
+const { Op, where } = require("sequelize");
 
 const AlunoController = {
   index: async (req, res) => {
-    let alunos = await Aluno.findAll();
+    let alunos = await Aluno.findAll(
+      {
+        include: [{association: 'turma', through: {atributes: 'matriculas'}}]
+      })
+
     res.status(200).json(alunos);
 
   },
   detalhes: async (req, res) => {
     let {id} = req.params;
-    let aluno = await Aluno.findByPk(id);
+    let aluno = await Aluno.findOne(
+      {
+        where: {id: id},
+        include: [{association: 'turma', through: {atributes: 'matriculas'}}]
+      })
     res.status(200).json(aluno)
+  },
+  buscar: async (req, res) => {
+    let {aluno} = req.query;
+
+    let alunosFiltrados = await Aluno.findAll(
+      {where: {nome: {[Op.substring]: `${aluno}`}}}
+    )
+    
+    res.status(200).json(alunosFiltrados);
+
   },
   editar: async (req, res) => {
     let {id} = req.params;
